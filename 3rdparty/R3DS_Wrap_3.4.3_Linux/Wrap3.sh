1acc8633d8d11a559f2f296cdd2a9333864503ef
Ivan Gubarev <ivan.gubarev@gmail.com>
2019-10-08 13:57:00 +0300

    add another test build

---
+++ 3rdparty/R3DS_Wrap_3.4.3_Linux/Wrap3.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:831b0255ba12893fd3e8cdb0db18069e1fc2bd99cafd7434889089579d1bf1d5
+size 1018
---
+++ 3rdparty/R3DS_Wrap_3.4.3_Linux/Wrap3Cmd.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b2748122102e1fa4e990729cb8921d8c6587dbdf97237420d8dfb46455a817c4
+size 1027
